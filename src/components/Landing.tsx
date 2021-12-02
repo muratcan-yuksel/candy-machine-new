fa66deedafa0c8182bc694f7c133f6072f05dd12
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-02 04:28:51 +0300

    add landing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "../styles/landing.css";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 const Landing = () => {
   return (
-    <div>
+    <div id="landingPage">
       <Container>
         <Row>
           <Col xs={12} sm={12} md={12} lg={6} xl={6} xxl={6}>
@@ -19,12 +19,27 @@ const Landing = () => {
                 <h2>Thousands of loveable llamas from the </h2>
                 <h1>llamadramaclub</h1>
                 <h2>are joining the NFT world January 2022.</h2>
+                <Button variant="warning">MINT</Button>{" "}
               </div>
             </div>
           </Col>
         </Row>
         <Row>
-          <Col>1 of 3</Col>
+          <Col>
+            <div id="LandingFollows">
+              <div id="followButtons">
+                <span className="followButton">
+                  <Button variant="warning">Twitter</Button>{" "}
+                </span>
+                <span className="followButton">
+                  <Button variant="warning">Facebook</Button>{" "}
+                </span>
+                <span className="followButton">
+                  <Button variant="warning">Instagram</Button>{" "}
+                </span>
+              </div>
+            </div>
+          </Col>
         </Row>
       </Container>
     </div>
