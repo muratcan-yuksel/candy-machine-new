465daaf733acb73e54db84a7594ef1aed038e46b
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-02 04:51:59 +0300

    IDK what I did but solved theoverlapping

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Button from "react-bootstrap/Button";
 import Landing from "./components/Landing";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
+import "./styles/app.css";
 
 const App = () => {
   return (
-    <div>
+    <div id="HomePage">
       <NavBar />
       <Landing />
+      <About />
     </div>
   );
 };
---
+++ src/components/About.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <div id="aboutPage">
+      <h1 id="aboutTitle">llamadramaclub</h1>
+      <p className="aboutText">
+        was created by a group of creatives with a passion for music,
+      </p>
+      <p className="aboutText">theater</p>
+      <p className="aboutText">
+        and animation. It is our goal to spearhead projects of various genres
+        and collab with our favorite artist who dabbling in art, short film
+        animation and gaming.
+      </p>
+    </div>
+  );
+};
+
+export default About;
