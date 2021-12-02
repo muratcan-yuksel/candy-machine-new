f9b8534c9849063b5b01497472570b99e719353f
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-02 11:23:01 +0300

    add roadmap again

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -4,90 +4,92 @@ import "../styles/roadmap.css";
 
 const Roadmap = () => {
   return (
-    <div>
+    <div id="roadmapComponent">
       <h1>RoadMap</h1>
       <div>
-        <Row>
-          <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
-            <div className="roadmapTextContainer">
-              <h2 className="roadmapTitle">Title</h2>
-              <p className="roadmapText">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Architecto adipisci debitis praesentium mollitia, sit eos
-                voluptates maxime ab quae a veritatis quas ex velit veniam
-                laborum esse reprehenderit exercitationem corrupti.
-              </p>
-            </div>
-          </Col>
-        </Row>{" "}
+        <Container>
+          <Row>
+            <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>{" "}
+            <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>{" "}
+            <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>{" "}
+            <Col xs={12} sm={12} md={3} lg={3} xl={3} xxl={3}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>{" "}
+            <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>{" "}
+            <Col xs={12} sm={12} md={4} lg={4} xl={4} xxl={4}>
+              <div className="roadmapTextContainer">
+                <h2 className="roadmapTitle">Title</h2>
+                <p className="roadmapText">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Architecto adipisci debitis praesentium mollitia, sit eos
+                  voluptates maxime ab quae a veritatis quas ex velit veniam
+                  laborum esse reprehenderit exercitationem corrupti.
+                </p>
+              </div>
+            </Col>
+          </Row>{" "}
+        </Container>
       </div>
     </div>
   );
