4a7abc652ea1274c1aaeb68026c6804a61dbdae2
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-02 10:58:11 +0300

    add middlepart

--- src/components/MiddlePart.tsx
+++ src/components/MiddlePart.tsx
@@ -12,7 +12,22 @@ const MiddlePart = () => {
           </div>
         </Col>
         <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6}>
-          <h1>text</h1>
+          <div id="middleText">
+            <h1>Title</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores
+              maxime ducimus doloribus, harum dolorum nulla unde illum animi
+              adipisci praesentium placeat! Ratione, fuga quos odio labore ut
+              quisquam libero nesciunt!
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id,
+              similique aut? Numquam sed voluptates in! Quisquam porro culpa
+              eaque, commodi architecto voluptate, beatae laboriosam earum
+              impedit odit dolorem adipisci facere.
+            </p>
+            <Button variant="dark">Dark</Button>{" "}
+          </div>
         </Col>
       </Row>
     </div>
