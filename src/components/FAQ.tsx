54c65b630f4cfb012efe2101ee50cf1a11af72aa
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-04 23:59:19 +0300

    faw animated

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -8,41 +8,49 @@ const FAQ = () => {
       <h1 id="faqCompTitle">FAQ</h1>
       <Container>
         <Row>
-          <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
-            <div className="faqTextContainer">
-              <h2 className="faqTitle">How do I get an NFT?</h2>
-              <p className="faqText">
-                Stay updated on all social media accounts. Presale and live mint
-                details will be announced with direct link to the action.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
-            <div className="faqTextContainer">
-              <h2 className="faqTitle">When will the llamas be available?</h2>
-              <p className="faqText">January 2022</p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
-            <div className="faqTextContainer">
-              <h2 className="faqTitle">How many NFTs can I buy?</h2>
-              <p className="faqText">
-                There is no limit. We believe that our mission will attract the
-                kind of investors that want to double down on us. Our goal is to
-                show investors they made the right choice.
-              </p>
-            </div>
-          </Col>{" "}
-          <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
-            <div className="faqTextContainer">
-              <h2 className="faqTitle">Why Sol?</h2>
-              <p className="faqText">
-                Simple… Value. LDC is a project that will kick butt in the video
-                game, art, and animation realms. The fractional gas fees allow
-                us to give you more value for your investment.
-              </p>
-            </div>
-          </Col>{" "}
+          <div data-aos="flip-up">
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+              <div className="faqTextContainer">
+                <h2 className="faqTitle">How do I get an NFT?</h2>
+                <p className="faqText">
+                  Stay updated on all social media accounts. Presale and live
+                  mint details will be announced with direct link to the action.
+                </p>
+              </div>
+            </Col>{" "}
+          </div>
+          <div data-aos="flip-up">
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+              <div className="faqTextContainer">
+                <h2 className="faqTitle">When will the llamas be available?</h2>
+                <p className="faqText">January 2022</p>
+              </div>
+            </Col>{" "}
+          </div>
+          <div data-aos="flip-up">
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+              <div className="faqTextContainer">
+                <h2 className="faqTitle">How many NFTs can I buy?</h2>
+                <p className="faqText">
+                  There is no limit. We believe that our mission will attract
+                  the kind of investors that want to double down on us. Our goal
+                  is to show investors they made the right choice.
+                </p>
+              </div>
+            </Col>{" "}
+          </div>
+          <div data-aos="flip-up">
+            <Col xs={12} sm={12} md={12} lg={12} xl={12} xxl={12}>
+              <div className="faqTextContainer">
+                <h2 className="faqTitle">Why Sol?</h2>
+                <p className="faqText">
+                  Simple… Value. LDC is a project that will kick butt in the
+                  video game, art, and animation realms. The fractional gas fees
+                  allow us to give you more value for your investment.
+                </p>
+              </div>
+            </Col>{" "}
+          </div>
         </Row>{" "}
       </Container>
     </div>
