812be64e8cac66c68e42d6527add8fa909a7ad83
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-12-04 22:27:16 +0300

    style about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,12 +4,9 @@ import "../styles/about.css";
 const About = () => {
   return (
     <div id="aboutPage">
-      <h1 id="aboutTitle">llamadramaclub</h1>
+      <h1 id="aboutTitle">LLAMADRAMACLUB</h1>
       <p className="aboutText">
-        was created by a group of creatives with a passion for music,
-      </p>
-      <p className="aboutText">theater</p>
-      <p className="aboutText">
+        was created by a group of creatives with a passion for music, theater
         and animation. It is our goal to spearhead projects of various genres
         and collab with our favorite artist who dabbling in art, short film
         animation and gaming.
